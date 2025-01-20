2a39491339261667cdf892c730b1eead6a8f063a
William Castro Ayaviri <wissindrako@gmail.com>
2025-01-19 22:24:13 -0400

    principal y login

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,15 +1,15 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         @isset($logo)
             {{ $logo }}
         @else
-            <Link href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
+            <h1 class="text-4xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold uppercase leading-tight tracking-tight">
+                <span class="text-yellow-500">Ingreso</span>
+            </h1>
         @endisset
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-300 dark:bg-gray-800 text-gray-100 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
